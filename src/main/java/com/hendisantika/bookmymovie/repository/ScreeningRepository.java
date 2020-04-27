6a69d732fa752fb60e5878be6905079435871306
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-28 04:39:24 +0700

    #14 - Add ScreeningRepository

---
+++ src/main/java/com/hendisantika/bookmymovie/repository/ScreeningRepository.java
@@ -0,0 +1,25 @@
+package com.hendisantika.bookmymovie.repository;
+
+import com.hendisantika.bookmymovie.entity.Screening;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : book-mymovie
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/04/20
+ * Time: 04.38
+ */
+public interface ScreeningRepository extends CrudRepository<Screening, Long> {
+    List<Screening> findByScreeningDate(Date screeningDate);
+
+    List<Screening> findByMovieName(String movieName);
+
+    Screening findByMovieNameAndTheatreIdAndScreeningDateAndScreeningTime(String movieName, long theatreId,
+                                                                          Date screeningDate, Time screeningTime);
+}
