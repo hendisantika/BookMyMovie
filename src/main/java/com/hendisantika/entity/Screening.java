5de9afb7af118572c7e21ffcc1c077dfc2c278b2
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-28 04:09:16 +0700

    #7 - Add Screening Entity

---
+++ src/main/java/com/hendisantika/entity/Screening.java
@@ -0,0 +1,55 @@
+package com.hendisantika.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : book-mymovie
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/04/20
+ * Time: 04.08
+ */
+@Entity
+@Table(name = "SCREENING")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Screening implements Cloneable {
+    @Id
+    @Column(name = "SCREENING_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long screeningId;
+
+    @Column(name = "THEATRE_ID")
+    private long theatreId;
+
+    @Column(name = "SCREEN_ID")
+    private long screenId;
+
+    @Column(name = "MOVIE_NAME")
+    private String movieName;
+
+    @Column(name = "SCREENING_DATE")
+    private java.sql.Date screeningDate;
+
+    @Column(name = "SCREENING_TIME")
+    private java.sql.Time screeningTime;
+
+    @Column(name = "BOOKED_TICKETS")
+    private int bookedTickets;
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
