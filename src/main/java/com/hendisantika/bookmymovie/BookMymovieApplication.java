55c983e50570ce58ffd82d3882ab200a3bcb7c66
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-28 03:58:28 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/bookmymovie/BookMymovieApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.bookmymovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMymovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMymovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/bookmymovie/BookMymovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.bookmymovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMymovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
