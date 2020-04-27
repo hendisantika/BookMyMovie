baf765f8ea02d89aea37a41d2e37cec268bccee2
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-28 04:31:24 +0700

    #11 - Add User Entity

---
+++ src/main/java/com/hendisantika/bookmymovie/entity/User.java
@@ -0,0 +1,39 @@
+package com.hendisantika.bookmymovie.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : book-mymovie
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/04/20
+ * Time: 04.30
+ */
+@Entity
+@Table(name = "USER")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @Column(name = "USER_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "USERNAME", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+}
