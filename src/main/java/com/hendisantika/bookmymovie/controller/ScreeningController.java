623f18b4b8f75d8fd0a6c40261d67fa2a9390b06
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-29 01:23:37 +0700

    #40 - Add bookSeats API in ScreeningController

--- src/main/java/com/hendisantika/bookmymovie/controller/ScreeningController.java
+++ src/main/java/com/hendisantika/bookmymovie/controller/ScreeningController.java
@@ -9,9 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -40,5 +43,26 @@ public class ScreeningController {
         model.addAttribute("screenings", result);
         model.addAttribute("movie", movieRepository.findByMovieName(movieString));
         return "screenings";
+    }
+
+    @PostMapping
+    public String bookSeats(@Valid @ModelAttribute MovieScreening movieBooking) {
+
+        LOGGER.info(movieBooking.getMovieName());
+        LOGGER.info(movieBooking.getTheatreCity());
+        LOGGER.info(movieBooking.getTheatreName());
+        LOGGER.info(movieBooking.getScreeningTime());
+        LOGGER.info(movieBooking.getScreeningDate());
+        LOGGER.info(Integer.toString(movieBooking.getNumSeats()));
+
+        int bookedSeats = this.screeningService.getBookedSeats(movieBooking);
+        int totalSeats = this.screeningService.getTotalSeats(movieBooking);
+
+        if ((bookedSeats + movieBooking.getNumSeats()) > totalSeats)
+            return "error";
+
+        this.screeningService.bookSeats(movieBooking, bookedSeats + movieBooking.getNumSeats());
+
+        return "result";
     }
 }
