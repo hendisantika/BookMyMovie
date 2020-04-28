ea69143af087eabac19384b5a5819d958a185395
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-29 01:07:37 +0700

    #18 - Add WebSecurityConfig

---
+++ src/main/java/com/hendisantika/bookmymovie/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.hendisantika.bookmymovie.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : book-mymovie
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/04/20
+ * Time: 01.07
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+}
