a185a7f0de1f7aa29479f7879aa299ef13e2a7bf
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-29 01:22:17 +0700

    #37 - Add getMovies API in MovieController

--- src/main/java/com/hendisantika/bookmymovie/controller/MovieController.java
+++ src/main/java/com/hendisantika/bookmymovie/controller/MovieController.java
@@ -1,14 +1,22 @@
 package com.hendisantika.bookmymovie.controller;
 
+import com.hendisantika.bookmymovie.business.domain.MovieScreening;
+import com.hendisantika.bookmymovie.entity.Movie;
 import com.hendisantika.bookmymovie.service.ScreeningService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +35,24 @@ public class MovieController {
 
     @Autowired
     private ScreeningService screeningService;
+
+    @GetMapping
+    public String getMovies(@RequestParam(value = "date", required = false) String dateString, Model model) {
+        Date date = null;
+        if (dateString != null) {
+            try {
+                date = DATE_FORMAT.parse(dateString);
+
+            } catch (ParseException pe) {
+                date = new Date();
+            }
+        } else {
+            date = new Date();
+        }
+
+        Set<Movie> result = this.screeningService.getMoviesByDate(date);
+        model.addAttribute("movies", result);
+        model.addAttribute("movieBooking", new MovieScreening());
+        return "movies";
+    }
 }
