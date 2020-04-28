93483985cc1d77b52ebe0c5ffb5467f0ee67a4a7
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-29 01:22:44 +0700

    #38 - Add ScreeningController

---
+++ src/main/java/com/hendisantika/bookmymovie/controller/ScreeningController.java
@@ -0,0 +1,30 @@
+package com.hendisantika.bookmymovie.controller;
+
+import com.hendisantika.bookmymovie.repository.MovieRepository;
+import com.hendisantika.bookmymovie.service.ScreeningService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : book-mymovie
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/04/20
+ * Time: 01.22
+ */
+@Controller
+@RequestMapping("/screenings")
+public class ScreeningController {
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private ScreeningService screeningService;
+}
