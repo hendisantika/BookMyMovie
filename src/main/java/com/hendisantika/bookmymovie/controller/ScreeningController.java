699526a35942eaabd250b675580d37b005e01153
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-29 01:23:12 +0700

    #39 - Add getScreenings API in ScreeningController

--- src/main/java/com/hendisantika/bookmymovie/controller/ScreeningController.java
+++ src/main/java/com/hendisantika/bookmymovie/controller/ScreeningController.java
@@ -1,12 +1,18 @@
 package com.hendisantika.bookmymovie.controller;
 
+import com.hendisantika.bookmymovie.business.domain.MovieScreening;
 import com.hendisantika.bookmymovie.repository.MovieRepository;
 import com.hendisantika.bookmymovie.service.ScreeningService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +33,12 @@ public class ScreeningController {
 
     @Autowired
     private ScreeningService screeningService;
+
+    @GetMapping
+    public String getScreenings(@RequestParam(value = "movie", required = true) String movieString, Model model) {
+        List<MovieScreening> result = this.screeningService.getMovieScreeningsByMovie(movieString);
+        model.addAttribute("screenings", result);
+        model.addAttribute("movie", movieRepository.findByMovieName(movieString));
+        return "screenings";
+    }
 }
