2ad043a1fa4cb8a2d530c4555ebcc9ace04e0e67
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-29 01:07:58 +0700

    #19 - Add bCryptPasswordEncoder bean in WebSecurityConfig

--- src/main/java/com/hendisantika/bookmymovie/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/bookmymovie/config/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.hendisantika.bookmymovie.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +22,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
