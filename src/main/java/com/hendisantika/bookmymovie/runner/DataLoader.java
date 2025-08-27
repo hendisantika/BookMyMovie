f96d6d05c831045bffedfc1e972d9583536c951a
hendisantika <hendisantika@yahoo.co.id>
2025-08-27 08:19:53 +0700

    fix: code issues :three:🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/bookmymovie/runner/DataLoader.java
+++ src/main/java/com/hendisantika/bookmymovie/runner/DataLoader.java
@@ -144,21 +144,12 @@ public class DataLoader implements ApplicationRunner {
     }
 
     private void populateScreeningsTable() throws CloneNotSupportedException {
-        /* schema.sql lists 5 theaters, generate 2 screenings randomly for
-         * each screen in each theater
-         */
-        for (int i = 1; i <= 5; i++) {
-            List<Screen> screens = screenRepository.findByTheatreId(i);
-            for (int j = 0; j < screens.size(); j++) {
-                Screening screening1 = new Screening();
-                Screening screening2 = new Screening();
+        /* Generate screenings for each screen in each theater */
+        for (int theatreId = 1; theatreId <= 5; theatreId++) {
+            List<Screen> screens = screenRepository.findByTheatreId(theatreId);
+            for (Screen screen : screens) {
 
-                screening1.setTheatreId(i);
-                screening1.setScreenId(j + 1);
-                screening2.setTheatreId(i);
-                screening2.setScreenId(j + 1);
-
-                // Randomly select 2 movies from the movies db, 1 each for each screen
+                // Get random movies
                 long totalMovies = movieRepository.count();
                 Random random = new Random();
 
@@ -172,40 +163,44 @@ public class DataLoader implements ApplicationRunner {
                 while ((movie2 = movieRepository.findByMovieId(movieId2)) == null)
                     movieId2 = random.nextInt((int) totalMovies) + 1;
 
-                screening1.setMovieId(movie1.getMovieId());
-                screening2.setMovieId(movie2.getMovieId());
+                // Get random dates
+                Date today = new Date((new java.util.Date()).getTime());
+                Date threeDaysLater = new Date(today.getTime() + 3 * 24 * 60 * 60 * 1000);
+                Date randomDate1 = new Date(ThreadLocalRandom.current().nextLong(today.getTime(), threeDaysLater.getTime()));
+                Date randomDate2 = new Date(ThreadLocalRandom.current().nextLong(today.getTime(), threeDaysLater.getTime()));
 
-                // Get a random date between current date and 3 days from current date
-                Date date1 = new Date((new java.util.Date()).getTime());
-                Date date2 = new Date(date1.getTime() + 3 * 24 * 60 * 60 * 1000);
-                Date randomDate1 = new Date(ThreadLocalRandom.current().nextLong(date1.getTime(), date2.getTime()));
-                Date randomDate2 = new Date(ThreadLocalRandom.current().nextLong(date1.getTime(), date2.getTime()));
+                // Create and save first screening at 10:00
+                saveScreeningIfNotExists(theatreId, screen.getScreenId(), movie1.getMovieId(), randomDate1, Time.valueOf("10:00:00"));
 
-                screening1.setScreeningDate(randomDate1);
-                screening2.setScreeningDate(randomDate2);
+                // Create and save second screening at 18:00 (same screen, date, theatre but different time)
+                saveScreeningIfNotExists(theatreId, screen.getScreenId(), movie1.getMovieId(), randomDate1, Time.valueOf("18:00:00"));
 
-                screening1.setBookedTickets(0);
-                screening2.setBookedTickets(0);
-
-                // 2 screenings per screen
-                screening1.setScreeningTime(Time.valueOf("10:00:00"));
-                screeningRepository.save(screening1);
-
-                Screening screening1Clone = (Screening) screening1.clone();
-                screening1.setScreeningTime(Time.valueOf("18:00:00"));
-                screeningRepository.save(screening1Clone);
-
-                if (randomDate1.getDate() != randomDate2.getDate()) {
-                    screening2.setScreeningTime(Time.valueOf("10:00:00"));
-                    screeningRepository.save(screening2);
-
-                    Screening screening2Clone = (Screening) screening2.clone();
-                    screening2.setScreeningTime(Time.valueOf("18:00:00"));
-                    screeningRepository.save(screening2Clone);
+                // Create screenings for second movie only if different date
+                if (randomDate1.getTime() != randomDate2.getTime()) {
+                    saveScreeningIfNotExists(theatreId, screen.getScreenId(), movie2.getMovieId(), randomDate2, Time.valueOf("10:00:00"));
+                    saveScreeningIfNotExists(theatreId, screen.getScreenId(), movie2.getMovieId(), randomDate2, Time.valueOf("18:00:00"));
                 }
             }
         }
     }
+
+    private void saveScreeningIfNotExists(long theatreId, long screenId, int movieId, Date date, Time time) {
+        // Check if screening already exists
+        Screening existing = screeningRepository.findByMovieIdAndTheatreIdAndScreeningDateAndScreeningTime(
+                movieId, theatreId, date, time);
+
+        if (existing == null) {
+            Screening screening = new Screening();
+            screening.setTheatreId(theatreId);
+            screening.setScreenId(screenId);
+            screening.setMovieId(movieId);
+            screening.setScreeningDate(date);
+            screening.setScreeningTime(time);
+            screening.setBookedTickets(0);
+            screeningRepository.save(screening);
+        }
+    }
+
 
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
