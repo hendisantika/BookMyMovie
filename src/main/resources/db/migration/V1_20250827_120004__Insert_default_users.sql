1ab25402331c65401f12ea3b7ad09091926b3b93
hendisantika <hendisantika@yahoo.co.id>
2025-10-21 10:47:17 +0700

    fix(sql): User Migration file🫸🌀✏️📗🐧🐳

--- src/main/resources/db/migration/V1_20250827_120004__Insert_default_users.sql
+++ src/main/resources/db/migration/V1_20250827_120004__Insert_default_users.sql
@@ -1,10 +1,10 @@
 -- Insert default users with BCrypt encoded passwords
 -- Password for all users is "53cret" (BCrypt encoded with strength 10)
 
-INSERT INTO USER (USERNAME, PASSWORD) VALUES
-('yuji', '$2a$10$DQzFHMPXzSzCJ3GczB1kRuBKnO7wlNUpNnRnP7vxvHnBv0pNYNmRC'),
-('geto', '$2a$10$DQzFHMPXzSzCJ3GczB1kRuBKnO7wlNUpNnRnP7vxvHnBv0pNYNmRC'),
-('gojo', '$2a$10$DQzFHMPXzSzCJ3GczB1kRuBKnO7wlNUpNnRnP7vxvHnBv0pNYNmRC'),
-('senku', '$2a$10$DQzFHMPXzSzCJ3GczB1kRuBKnO7wlNUpNnRnP7vxvHnBv0pNYNmRC'),
-('yaiba', '$2a$10$DQzFHMPXzSzCJ3GczB1kRuBKnO7wlNUpNnRnP7vxvHnBv0pNYNmRC'),
-('tanjiro', '$2a$10$DQzFHMPXzSzCJ3GczB1kRuBKnO7wlNUpNnRnP7vxvHnBv0pNYNmRC');
+INSERT INTO USER (USERNAME, PASSWORD) VALUES ('yuji', '$2a$12$Cs78t9qlBzawm6TNJQ8bFuK8K9KOU4cA2uA6xDGvTnltVMQo4yUke'),
+                                             ('geto', '$2a$12$Cs78t9qlBzawm6TNJQ8bFuK8K9KOU4cA2uA6xDGvTnltVMQo4yUke'),
+                                             ('gojo', '$2a$12$Cs78t9qlBzawm6TNJQ8bFuK8K9KOU4cA2uA6xDGvTnltVMQo4yUke'),
+                                             ('senku', '$2a$12$Cs78t9qlBzawm6TNJQ8bFuK8K9KOU4cA2uA6xDGvTnltVMQo4yUke'),
+                                             ('yaiba', '$2a$12$Cs78t9qlBzawm6TNJQ8bFuK8K9KOU4cA2uA6xDGvTnltVMQo4yUke'),
+                                             ('tanjiro',
+                                              '$2a$12$Cs78t9qlBzawm6TNJQ8bFuK8K9KOU4cA2uA6xDGvTnltVMQo4yUke');
